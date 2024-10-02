d465c159a8d674d8faca13a87cde5b503402f774
JLEVEM01 <jlevem23@gmail.com>
2024-10-02 15:54:44 -0600

    Commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+            },
+            backgroundImage: {
+                "bgIndex": "url('/public/img/Backgrounds/bgIndex2.jpg')"
+            }
+
+        }
+    }
+}
